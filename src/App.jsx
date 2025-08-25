625cd9acf06280c0f0a0cdcef8d3adb7a09097f3
Jaisung <jaisung.lidder@gmail.com>
2025-08-24 21:21:54 -0700

    changed the index to the root

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// src/App.jsx
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Call of Cthulhu Character Sheet</h1>
+      <p>Welcome, investigator. Build your character here.</p>
+    </div>
+  );
+}
+
+export default App;
