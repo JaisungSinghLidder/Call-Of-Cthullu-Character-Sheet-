432e42539978c0cb5579b89d7ae629f6075a2239
Jaisung <jaisung.lidder@gmail.com>
2025-09-08 18:54:36 -0700

    now the character skills uses the context and debugged and fixed an error where Math was mispelled as Max

--- src/components/CharacterSkills.jsx
+++ src/components/CharacterSkills.jsx
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useContext } from "react";
 import { useState } from "react";
 import "./CharacterSkills.css";
+import { CharacterContext } from "./CharacterContext";
+
 
 function CharacterSkills() {
+    /*
     const [strength, setStrength] = useState("");
     const [dexterity, setDexterity] = useState("");
     const [constitution, setConstitution] = useState("");
@@ -12,6 +15,10 @@ function CharacterSkills() {
     const [size, setSize] = useState("");
     const [education, setEducation] = useState("");
     const [luck, setLuck] = useState("");
+    */
+
+    const { coreStats, setCoreStats } = useContext(CharacterContext);
+
 
 
     const [currentHP, setCurrentHP] = useState(null);
@@ -22,6 +29,7 @@ function CharacterSkills() {
 
     const [currentMagic, setCurrentMagic] = useState(null);
     const [changeAmountMagic, setChangeAmountMagic] = useState("");
+    
 
 
     //generating rolling stats 
@@ -41,24 +49,27 @@ function CharacterSkills() {
         return total * 5;
     }
 
-    //now applying those stats
     const rollAllStats = () => {
-        setStrength(roll3d6Times5());
-        setDexterity(roll3d6Times5());
-        setConstitution(roll3d6Times5());
-        setIntelligence(roll2d6Times5());
-        setAppearance(roll3d6Times5());
-        setPower(roll3d6Times5());
-        setSize(roll2d6Times5());
-        setEducation(roll2d6Times5());  
-        setLuck(roll3d6Times5)
+        const newState = {
+            strength : roll3d6Times5(),
+            dexterity : roll3d6Times5(),
+            constitution : roll3d6Times5(),
+            intelligence : roll2d6Times5(),
+            appearance : roll3d6Times5(),
+            power : roll3d6Times5(),
+            size : roll2d6Times5(),
+            education : roll2d6Times5(),
+            luck : roll3d6Times5()
+        };
+
+        setCoreStats(newState);
     };
 
     //here will set the stats if they exist
     //these stats are derived from these core stats
-    const hitPoints = constitution && size ? Math.floor((constitution + size) / 10) : ""; 
-    const sanity = power || "";
-    const magicPoints = power || "";
+    const hitPoints = coreStats.constitution && coreStats.size ? Math.floor((coreStats.constitution + coreStats.size) / 10) : ""; 
+    const sanity = coreStats.power || "";
+    const magicPoints = coreStats.power || "";
 
 
     React.useEffect(() => {
@@ -108,7 +119,7 @@ function CharacterSkills() {
     const handleRestore = () => {
         if (changeAmountMagic)
         {
-            setCurrentMagic(prev => Max.min(magicPoints, prev + Number(changeAmountMagic)));
+            setCurrentMagic(prev => Math.min(magicPoints, prev + Number(changeAmountMagic)));
             setChangeAmountMagic("");
         }
     };
@@ -145,48 +156,48 @@ function CharacterSkills() {
             <div className="core-stats">
 
                 <div className="stat-block">
-                <label>STR: <input type = "number" value={strength} onChange={e => setStrength(e.target.value)} /></label>
-                <p>½: {strength ? Math.floor(strength / 2) : ""}, ⅕: {strength ? Math.floor(strength / 5) : ""}</p>
+                <label>STR: <input type = "number" value={coreStats.strength} onChange={e => setCoreStats(prev => ({...prev, strength:Number(e.target.value)})) } /></label>
+                <p>½: {coreStats.strength ? Math.floor(coreStats.strength / 2) : ""}, ⅕: {coreStats.strength ? Math.floor(coreStats.strength / 5) : ""}</p>
                 </div>
 
                 <div className="stat-block">
-                <label>DEX: <input type = "number" value={dexterity} onChange={e => setDexterity(e.target.value)} /></label>
-                <p>½: {dexterity ? Math.floor(dexterity / 2) : ""}, ⅕: {dexterity ? Math.floor(dexterity / 5) : ""}</p>
+                <label>DEX: <input type = "number" value={coreStats.dexterity} onChange={e => setCoreStats(prev => ({...prev, dexterity:Number(e.target.value)})) } /></label>
+                <p>½: {coreStats.dexterity ? Math.floor(coreStats.dexterity / 2) : ""}, ⅕: {coreStats.dexterity ? Math.floor(coreStats.dexterity / 5) : ""}</p>
                 </div>
 
                 <div className="stat-block">
-                <label>CON: <input type = "number" value={constitution} onChange={e => setConstitution(e.target.value)} /></label>
-                <p>½: {constitution ? Math.floor(constitution / 2) : ""}, ⅕: {constitution ? Math.floor(constitution / 5) : ""}</p>
+                <label>CON: <input type = "number" value={coreStats.constitution} onChange={e => setCoreStats(prev => ({...prev, constitution:Number(e.target.value)})) } /></label>
+                <p>½: {coreStats.constitution ? Math.floor(coreStats.constitution / 2) : ""}, ⅕: {coreStats.constitution ? Math.floor(coreStats.constitution / 5) : ""}</p>
                 </div>
 
                 <div className="stat-block">
-                <label>INT: <input type = "number" value={intelligence} onChange={e => setIntelligence(e.target.value)} /></label>
-                <p>½: {intelligence ? Math.floor(intelligence / 2) : ""}, ⅕: {intelligence ? Math.floor(intelligence / 5) : ""}</p>
+                <label>INT: <input type = "number" value={coreStats.intelligence} onChange={e => setCoreStats(prev => ({...prev, intelligence:Number(e.target.value)})) } /></label>
+                <p>½: {coreStats.intelligence ? Math.floor(coreStats.intelligence / 2) : ""}, ⅕: {coreStats.intelligence ? Math.floor(coreStats.intelligence / 5) : ""}</p>
                 </div>
 
                 <div className="stat-block">
-                <label>APP: <input type = "number" value={appearance} onChange={e => setAppearance(e.target.value)} /></label>
-                <p>½: {appearance ? Math.floor(appearance / 2) : ""}, ⅕: {appearance ? Math.floor(appearance / 5) : ""}</p>
+                <label>APP: <input type = "number" value={coreStats.appearance} onChange={e => setCoreStats(prev => ({...prev, appearance:Number(e.target.value)})) } /></label>
+                <p>½: {coreStats.appearance ? Math.floor(coreStats.appearance / 2) : ""}, ⅕: {coreStats.appearance ? Math.floor(coreStats.appearance / 5) : ""}</p>
                 </div>
 
                 <div className="stat-block">
-                <label>POW: <input type = "number" value={power} onChange={e => setPower(e.target.value)} /></label>
-                <p>½: {power ? Math.floor(power / 2) : ""}, ⅕: {power ? Math.floor(power / 5) : ""}</p>
+                <label>POW: <input type = "number" value={coreStats.power} onChange={e => setCoreStats(prev => ({...prev, power:Number(e.target.value)})) } /></label>
+                <p>½: {coreStats.power ? Math.floor(coreStats.power / 2) : ""}, ⅕: {coreStats.power ? Math.floor(coreStats.power / 5) : ""}</p>
                 </div>
 
                 <div className="stat-block">
-                <label>SIZ: <input type = "number" value={size} onChange={e => setSize(e.target.value)} /></label>
-                <p>½: {size ? Math.floor(size / 2) : ""}, ⅕: {size ? Math.floor(size / 5) : ""}</p>
+                <label>SIZ: <input type = "number" value={coreStats.size} onChange={e => setCoreStats(prev => ({...prev, size:Number(e.target.value)})) } /></label>
+                <p>½: {coreStats.size ? Math.floor(coreStats.size / 2) : ""}, ⅕: {coreStats.size ? Math.floor(coreStats.size / 5) : ""}</p>
                 </div>
 
                 <div className="stat-block">
-                <label>EDU: <input type = "number" value={education} onChange={e => setEducation(e.target.value)} /></label>
-                <p>½: {education ? Math.floor(education / 2) : ""}, ⅕: {education ? Math.floor(education / 5) : ""}</p>
+                <label>EDU: <input type = "number" value={coreStats.education} onChange={e => setCoreStats(prev => ({...prev, education:Number(e.target.value)})) } /></label>
+                <p>½: {coreStats.education ? Math.floor(coreStats.education / 2) : ""}, ⅕: {coreStats.education ? Math.floor(coreStats.education / 5) : ""}</p>
                 </div>
 
                 <div className="stat-block">
-                <label>LCK: <input type = "number" value={luck} onChange={e => setLuck(e.target.value)} /></label>
-                <p>½: {luck ? Math.floor(luck / 2) : ""}, ⅕: {luck ? Math.floor(luck / 5) : ""}</p>
+                <label>LCK: <input type = "number" value={coreStats.luck} onChange={e => setCoreStats(prev => ({...prev, luck:Number(e.target.value)})) } /></label>
+                <p>½: {coreStats.luck ? Math.floor(coreStats.luck / 2) : ""}, ⅕: {coreStats.luck ? Math.floor(coreStats.luck / 5) : ""}</p>
                 </div>
 
             </div>
