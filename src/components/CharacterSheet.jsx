903799ce5cbb31ea507eb174f7994d0b04942dbc
Jaisung <jaisung.lidder@gmail.com>
2025-09-08 18:07:57 -0700

    encapsulate the other components in the character sheet so they can have access to the context

--- src/components/CharacterSheet.jsx
+++ src/components/CharacterSheet.jsx
@@ -3,6 +3,8 @@ import { useState } from "react";
 import CharacterInfo from "./CharacterInfo.jsx";
 import CharacterSkills from "./CharacterSkills.jsx";
 import Characteristics from "./Characteristics.jsx";
+import { CharacterProvider } from "./CharacterContext";
+
 
 
 
@@ -11,9 +13,11 @@ import Characteristics from "./Characteristics.jsx";
 function CharacterSheet() {
   return (
     <div className="character-sheet">
-      <CharacterInfo />
-      <CharacterSkills />
-      <Characteristics />
+      <CharacterProvider>
+        <CharacterInfo />
+        <CharacterSkills />
+        <Characteristics />
+      </CharacterProvider>
     </div>
   );
 }
