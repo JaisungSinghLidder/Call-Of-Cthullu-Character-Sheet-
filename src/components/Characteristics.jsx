e9f38b89bc62d7885bf22a2408602d2fd25f2d6f
Jaisung <jaisung.lidder@gmail.com>
2025-09-22 12:26:05 -0700

    added a limit to the max amount of occupational skills you can add

--- src/components/Characteristics.jsx
+++ src/components/Characteristics.jsx
@@ -3,6 +3,9 @@ import "./Characteristics.css";
 import { CharacterContext } from "./CharacterContext";
 import occupationData from "./occupations.json";
 
+//This is for each of the skills of the player.
+//This could will allow the player to select serveal skills and calculate their occupation and personal skill points. 
+
 function Characteristics() {
   const { coreStats, setCoreStats, occupation, setOccupation } =
     useContext(CharacterContext);
@@ -68,6 +71,12 @@ function Characteristics() {
     customNine: 0,
   });
 
+  //In Call of Cthullu, you are only allowed a max of 8 occupational skills, so we check here
+  const [checkedOccupationalSkillsChecked, setOccupationalSkillsChecked] = useState([]);
+
+  const MAX_CHECKS = 8; 
+
+
   function calculateEducationPoints() {
     if (!occupationData) {
       console.log("ERROR! THERE APPEARS TO BE NO INFORMATION ABOUT THE OCCUPATIONS");
@@ -95,11 +104,34 @@ function Characteristics() {
     return coreStats.intelligence * 2;
   }
 
-  function Skill({ name, value }) {
+  function handleOccupationCheck(skillName)
+  {
+    setOccupationalSkillsChecked((prev) => {
+      if (prev.includes(skillName))
+      {
+        return prev.filter((s) => s !== skillName);
+      }
+      else if (prev.length < MAX_CHECKS)
+      {
+        return [...prev, skillName];
+      }
+      else
+      {
+        alert(`You can only select up to ${MAX_CHECKS} skills.`);
+        return prev;
+      }
+    })
+  }
+
+  //reusable function to make the creation more modular and cleaner
+  function Skill({ name, value, checked, onChange }) {
         return (
             <div className="skill-div">
             <label>
-                <input type="checkbox" />
+                <input type="checkbox"
+                 checked={checked} 
+                 onChange={() => onChange(name)}
+                />
                 {name} {value + "%"}
             </label>
             <div className="skill-boxes">
@@ -123,73 +155,73 @@ function Characteristics() {
 
         <div className="characteristicsColumn">
 
-            <Skill name="Accounting" value={skills.accounting} />
-            <Skill name="Anthropology" value={skills.anthropology} /> 
-            <Skill name="Appraise" value={skills.appraise} />
-            <Skill name="Archaeology" value={skills.archaeology} />
-            <Skill name="CustomArt" value={skills.customArt} />
-            <Skill name="CustomOne" value={skills.customOne} />
-            <Skill name="Charm" value={skills.charm} />
-            <Skill name="Climb" value={skills.climb} />
-            <Skill name="Credit Rating" value={skills.creditRating} />
-            <Skill name="Cthulhu Mythos" value={skills.cthulluMythos} />
-            <Skill name="Disguise" value={skills.disguise} />
-            <Skill name="Dodge" value={skills.dodge} />
-            <Skill name="Drive Auto" value={skills.driveAuto} />
-            <Skill name="Elec. Repair" value={skills.elecRepair} />
-            <Skill name="Fast Talk" value={skills.fastTalk} />
-            <Skill name="Fighting(Brawl)" value={skills.fightingBrawl} />
-            <Skill name="CustomeFightingOne" value={skills.fightingCustomOne} />
-            <Skill name="CustomFightingTwo" value={skills.fightingCustomTwo} />  
-            <Skill name="Firearms(Handgun)" value={skills.firearmsHandgun} />
+            <Skill name="Accounting" value={skills.accounting} checked={checkedOccupationalSkillsChecked.includes("Accounting") } onChange = {handleOccupationCheck} />
+            <Skill name="Anthropology" value={skills.anthropology} checked={checkedOccupationalSkillsChecked.includes("Anthropology") } onChange = {handleOccupationCheck} /> 
+            <Skill name="Appraise" value={skills.appraise} checked={checkedOccupationalSkillsChecked.includes("Appraise") } onChange = {handleOccupationCheck} />
+            <Skill name="Archaeology" value={skills.archaeology} checked={checkedOccupationalSkillsChecked.includes("Archaeology") } onChange = {handleOccupationCheck} />
+            <Skill name="CustomArt" value={skills.customArt} checked={checkedOccupationalSkillsChecked.includes("CustomArt") } onChange = {handleOccupationCheck} />
+            <Skill name="CustomOne" value={skills.customOne} checked={checkedOccupationalSkillsChecked.includes("CustomOne") } onChange = {handleOccupationCheck} />
+            <Skill name="Charm" value={skills.charm} checked={checkedOccupationalSkillsChecked.includes("Charm") } onChange = {handleOccupationCheck} />
+            <Skill name="Climb" value={skills.climb} checked={checkedOccupationalSkillsChecked.includes("Climb") } onChange = {handleOccupationCheck} />
+            <Skill name="Credit Rating" value={skills.creditRating} checked={checkedOccupationalSkillsChecked.includes("Credit Rating") } onChange = {handleOccupationCheck} />
+            <Skill name="Cthulhu Mythos" value={skills.cthulluMythos} checked={checkedOccupationalSkillsChecked.includes("Cthulhu Mythos") } onChange = {handleOccupationCheck} />
+            <Skill name="Disguise" value={skills.disguise} checked={checkedOccupationalSkillsChecked.includes("Disguise") } onChange = {handleOccupationCheck} />
+            <Skill name="Dodge" value={skills.dodge} checked={checkedOccupationalSkillsChecked.includes("Dodge") } onChange = {handleOccupationCheck} />
+            <Skill name="Drive Auto" value={skills.driveAuto} checked={checkedOccupationalSkillsChecked.includes("Drive Auto") } onChange = {handleOccupationCheck} />
+            <Skill name="Elec. Repair" value={skills.elecRepair} checked={checkedOccupationalSkillsChecked.includes("Elec. Repair") } onChange = {handleOccupationCheck} />
+            <Skill name="Fast Talk" value={skills.fastTalk} checked={checkedOccupationalSkillsChecked.includes("Fast Talk") } onChange = {handleOccupationCheck} />
+            <Skill name="Fighting(Brawl)" value={skills.fightingBrawl} checked={checkedOccupationalSkillsChecked.includes("Fighting(Brawl)") } onChange = {handleOccupationCheck} />
+            <Skill name="CustomeFightingOne" value={skills.fightingCustomOne} checked={checkedOccupationalSkillsChecked.includes("CustomeFightingOne") } onChange = {handleOccupationCheck} />
+            <Skill name="CustomFightingTwo" value={skills.fightingCustomTwo} checked={checkedOccupationalSkillsChecked.includes("CustomFightingTwo") } onChange = {handleOccupationCheck} />  
+            <Skill name="Firearms(Handgun)" value={skills.firearmsHandgun} checked={checkedOccupationalSkillsChecked.includes("Firearms(Handgun)") } onChange = {handleOccupationCheck} />
             
         </div>
 
         <div className="characteristicsColumn">
 
-            <Skill name="Firearms (Rifle/Shotgun)" value={skills.fireArmsRifle} />
-            <Skill name="CustomFirearms" value={skills.firearmsCustom} /> 
-            <Skill name="First Aid" value={skills.firstAid} />
-            <Skill name="History" value={skills.history} />
-            <Skill name="Intimidate" value={skills.intimidate} />
-            <Skill name="Jump" value={skills.jump} />
-            <Skill name="Language(Other)" value={skills.languageOther} />
-            <Skill name="CustomTwo" value={skills.customTwo} />
-            <Skill name="CustomThree" value={skills.customThree} />
-            <Skill name="Language (Own)" value={skills.languageOwn} />
-            <Skill name="Law" value={skills.law} />
-            <Skill name="Library Use" value={skills.libraryUse} />
-            <Skill name="Listen" value={skills.listen} />
-            <Skill name="Locksmith" value={skills.locksmith} />
-            <Skill name="Mech. Repair" value={skills.mechRepair} />
-            <Skill name="Medicine" value={skills.medicine} />
-            <Skill name="Natural World" value={skills.naturalWorld} />
-            <Skill name="Navigate" value={skills.navigate} />  
-            <Skill name="Occult" value={skills.occult} />
+            <Skill name="Firearms (Rifle/Shotgun)" value={skills.fireArmsRifle} checked={checkedOccupationalSkillsChecked.includes("Firearms (Rifle/Shotgun)") } onChange = {handleOccupationCheck} />
+            <Skill name="CustomFirearms" value={skills.firearmsCustom} checked={checkedOccupationalSkillsChecked.includes("CustomFirearms") } onChange = {handleOccupationCheck} /> 
+            <Skill name="First Aid" value={skills.firstAid} checked={checkedOccupationalSkillsChecked.includes("First Aid") } onChange = {handleOccupationCheck} />
+            <Skill name="History" value={skills.history} checked={checkedOccupationalSkillsChecked.includes("History") } onChange = {handleOccupationCheck} />
+            <Skill name="Intimidate" value={skills.intimidate} checked={checkedOccupationalSkillsChecked.includes("Intimidate") } onChange = {handleOccupationCheck} />
+            <Skill name="Jump" value={skills.jump} checked={checkedOccupationalSkillsChecked.includes("Jump") } onChange = {handleOccupationCheck} />
+            <Skill name="Language(Other)" value={skills.languageOther} checked={checkedOccupationalSkillsChecked.includes("Language(Other)") } onChange = {handleOccupationCheck} />
+            <Skill name="CustomTwo" value={skills.customTwo} checked={checkedOccupationalSkillsChecked.includes("CustomTwo") } onChange = {handleOccupationCheck} />
+            <Skill name="CustomThree" value={skills.customThree} checked={checkedOccupationalSkillsChecked.includes("CustomThree") } onChange = {handleOccupationCheck} />
+            <Skill name="Language (Own)" value={skills.languageOwn} checked={checkedOccupationalSkillsChecked.includes("Language (Own)") } onChange = {handleOccupationCheck} />
+            <Skill name="Law" value={skills.law} checked={checkedOccupationalSkillsChecked.includes("Law") } onChange = {handleOccupationCheck} />
+            <Skill name="Library Use" value={skills.libraryUse} checked={checkedOccupationalSkillsChecked.includes("Library Use") } onChange = {handleOccupationCheck} />
+            <Skill name="Listen" value={skills.listen} checked={checkedOccupationalSkillsChecked.includes("Listen") } onChange = {handleOccupationCheck} />
+            <Skill name="Locksmith" value={skills.locksmith} checked={checkedOccupationalSkillsChecked.includes("Locksmith") } onChange = {handleOccupationCheck} />
+            <Skill name="Mech. Repair" value={skills.mechRepair} checked={checkedOccupationalSkillsChecked.includes("Mech. Repair") } onChange = {handleOccupationCheck} />
+            <Skill name="Medicine" value={skills.medicine} checked={checkedOccupationalSkillsChecked.includes("Medicine") } onChange = {handleOccupationCheck} />
+            <Skill name="Natural World" value={skills.naturalWorld} checked={checkedOccupationalSkillsChecked.includes("Natural World") } onChange = {handleOccupationCheck} />
+            <Skill name="Navigate" value={skills.navigate} checked={checkedOccupationalSkillsChecked.includes("Navigate") } onChange = {handleOccupationCheck}/>  
+            <Skill name="Occult" value={skills.occult} checked={checkedOccupationalSkillsChecked.includes("Occult") } onChange = {handleOccupationCheck}/>
 
         </div>
 
         <div className="characteristicsColumn">
 
-            <Skill name="Persuade" value={skills.persuade} />
-            <Skill name="CustomFour" value={skills.customFour} /> 
-            <Skill name="Psychoanalysis" value={skills.psychoanalysis} />
-            <Skill name="Psychology" value={skills.psychology} />
-            <Skill name="Ride" value={skills.ride} />
-            <Skill name="Science" value={skills.scienceCustom} />
-            <Skill name="CustomFour" value={skills.customFour} />
-            <Skill name="CustomFifth" value={skills.customFifth} />
-            <Skill name="Sleight of Hand" value={skills.sleightOfHand} />
-            <Skill name="Spot Hidden" value={skills.spotHidden} />
-            <Skill name="Stealth" value={skills.stealth} />
-            <Skill name="Survival" value={skills.survivalCustom} />
-            <Skill name="Swim" value={skills.swim} />
-            <Skill name="Throw" value={skills.throw} />
-            <Skill name="Track" value={skills.track} />
-            <Skill name="CustomSix" value={skills.customSix} />
-            <Skill name="CustomSeven" value={skills.customSeven} />
-            <Skill name="CustomEight" value={skills.customEight} />  
-            <Skill name="CustomNine" value={skills.customNine} />
+            <Skill name="Persuade" value={skills.persuade} checked={checkedOccupationalSkillsChecked.includes("Persuade") } onChange = {handleOccupationCheck} />
+            <Skill name="CustomFour" value={skills.customFour} checked={checkedOccupationalSkillsChecked.includes("CustomFour") } onChange = {handleOccupationCheck} /> 
+            <Skill name="Psychoanalysis" value={skills.psychoanalysis} checked={checkedOccupationalSkillsChecked.includes("Psychoanalysis") } onChange = {handleOccupationCheck}/>
+            <Skill name="Psychology" value={skills.psychology} checked={checkedOccupationalSkillsChecked.includes("Psychology") } onChange = {handleOccupationCheck} />
+            <Skill name="Ride" value={skills.ride} checked={checkedOccupationalSkillsChecked.includes("Ride") } onChange = {handleOccupationCheck} />
+            <Skill name="Science" value={skills.scienceCustom} checked={checkedOccupationalSkillsChecked.includes("Science") } onChange = {handleOccupationCheck} />
+            <Skill name="CustomFour" value={skills.customFour} checked={checkedOccupationalSkillsChecked.includes("CustomFour") } onChange = {handleOccupationCheck} />
+            <Skill name="CustomFifth" value={skills.customFifth} checked={checkedOccupationalSkillsChecked.includes("CustomFifth") } onChange = {handleOccupationCheck}/>
+            <Skill name="Sleight of Hand" value={skills.sleightOfHand} checked={checkedOccupationalSkillsChecked.includes("Sleight of Hand") } onChange = {handleOccupationCheck} />
+            <Skill name="Spot Hidden" value={skills.spotHidden} checked={checkedOccupationalSkillsChecked.includes("Spot Hidden") } onChange = {handleOccupationCheck} />
+            <Skill name="Stealth" value={skills.stealth} checked={checkedOccupationalSkillsChecked.includes("Stealth") } onChange = {handleOccupationCheck} />
+            <Skill name="Survival" value={skills.survivalCustom} checked={checkedOccupationalSkillsChecked.includes("Survival") } onChange = {handleOccupationCheck} />
+            <Skill name="Swim" value={skills.swim} checked={checkedOccupationalSkillsChecked.includes("Swim") } onChange = {handleOccupationCheck}/>
+            <Skill name="Throw" value={skills.throw} checked={checkedOccupationalSkillsChecked.includes("Throw") } onChange = {handleOccupationCheck} />
+            <Skill name="Track" value={skills.track} checked={checkedOccupationalSkillsChecked.includes("Track") } onChange = {handleOccupationCheck} />
+            <Skill name="CustomSix" value={skills.customSix} checked={checkedOccupationalSkillsChecked.includes("CustomSix") } onChange = {handleOccupationCheck} />
+            <Skill name="CustomSeven" value={skills.customSeven} checked={checkedOccupationalSkillsChecked.includes("CustomSeven") } onChange = {handleOccupationCheck} />
+            <Skill name="CustomEight" value={skills.customEight} checked={checkedOccupationalSkillsChecked.includes("CustomEight") } onChange = {handleOccupationCheck} />  
+            <Skill name="CustomNine" value={skills.customNine} checked={checkedOccupationalSkillsChecked.includes("CustomNine") } onChange = {handleOccupationCheck} />
 
         </div>
 
